70014f0f3f9765c1ab5d74ce901ec4ade1f1330e
asunawesker <zs19004913@estudiantes.uv.mx>
2022-08-14 14:44:23 -0500

    testing contact form

--- src/app/components/contact/contact.component.spec.ts
+++ src/app/components/contact/contact.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ContactComponent } from './contact.component';
 
@@ -8,6 +11,11 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
       declarations: [ ContactComponent ]
     })
     .compileComponents();
@@ -17,5 +25,44 @@ describe('ContactComponent', () => {
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create a form with four controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+    expect(component.form.contains('phone')).toBeTruthy();
+    expect(component.form.contains('message')).toBeTruthy();
+  });
+
+  it('name should be required', () => {
+      let control : any;
+      control = component.form.get('name');
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+  });
+
+  it('email should be required', () => {
+      let control : any;
+      control = component.form.get('email');
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+  });
+
+  it('phone should be required', () => {
+      let control : any;
+      control = component.form.get('phone');
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+  });
+
+  it('message should be required', () => {
+      let control : any;
+      control = component.form.get('message');
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
   });
 });
--- src/app/components/properties/properties.component.spec.ts
+++ src/app/components/properties/properties.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PropertiesComponent } from './properties.component';
 
@@ -8,6 +10,10 @@ describe('PropertiesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       declarations: [ PropertiesComponent ]
     })
     .compileComponents();
